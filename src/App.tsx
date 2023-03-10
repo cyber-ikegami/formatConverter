05853eb90597e99dac52633901dd3a17864c8694
cyber-ikegami <ikegami@cyberbeing.tech>
2023-03-10 16:32:29 +0900

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,139 +1,106 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
+import StylesUtil from './stylesUtil';
+import SystemUtil from './systemUtil';
 
-const line = '--------------------------------------------';
-
-const App = ()=> {
+const App = () => {
+  const [formatValue, setFormatValue] = useState<string>('');
+  const [columnReplaseValue, setColumnReplaseValue] = useState<string>('\\t');
+  const [recordReplaseValue, setRecordReplaseValue] = useState<string>('\\n');
   const [baseValue, setBaseValue] = useState<string>('');
-  const [isDialog, setDialog] = useState<boolean>(false);
-  const [selectedValue, setSelectedValue] = useState<string>();
-  const [date, setDate] = useState<string>(getSystemDate());
-  const [affiliation, setAffiliation] = useState<string>('');
-  const [name, setName] = useState<string>('');
   const [showValue, setShowValue] = useState<string>('');
-  const [dateHistoryList, setDateHistoryList] = useState<string[]>([]);
-  const [affiliationHistoryList, setAffiliationHistoryList] = useState<string[]>([]);
-  const [nameHistoryList, setNameHistoryList] = useState<string[]>([]);
+  const [isDialogDisplay, setIsDialogDisplay] = useState<boolean>(false);
 
-  // 日付履歴JSXのリスト
-  const dataHistoryJsxList: JSX.Element[] = [];
-  for(let i = 0; i < dateHistoryList.length; i++){
-    dataHistoryJsxList.push(
-      <option>{dateHistoryList[i]}</option>
-    );
-  }
+  // 変換ボタン押下時の処理
+  const convert = () => {
+    const recordRegExpValue = new RegExp(`${recordReplaseValue}`);
+    const columnRegExpValue = new RegExp(`${columnReplaseValue}`);
 
-  // 所属履歴JSXのリスト
-  const affiliationHistoryJsxList: JSX.Element[] = [];
-  for(let i = 0; i < affiliationHistoryList.length; i++){
-    affiliationHistoryJsxList.push(
-      <option>{affiliationHistoryList[i]}</option>
-    );
-  }
-  
-  // 名前履歴JSXのリスト
-  const nameHistoryJsxList: JSX.Element[] = [];
-  for(let i = 0; i < nameHistoryList.length; i++){
-    nameHistoryJsxList.push(
-      <option>{nameHistoryList[i]}</option>
-    );
-  }
+    const lines = baseValue.split(recordRegExpValue);
+    const columnNames = lines[0].split(columnRegExpValue);
+    let result = '';
 
-  // プレビューに表示する値(所属の入力があれば括弧をつける)
-  let previewValue = '';
-  if(affiliation != '') {
-    previewValue = `${date} ${affiliation})${name}\n\n${selectedValue}\n${line}\n`;
-  } else {
-    previewValue = `${date} ${name}\n\n${selectedValue}\n${line}\n`;
+    lines.forEach((line, i) => {
+      if (i > 0) {
+        const splitLine = line.split(columnRegExpValue);
+        let base = formatValue;
+
+        columnNames.forEach((columnName, j) => {
+          let arg = `\\$${columnName}`;
+          base = base.replace(eval(`/${arg}/g`), splitLine[j]);
+        })
+        result = `${result}${base}\n`;
+      }
+    })
+    setShowValue(result);
+  };
+
+  // コピーボタン押下時の処理
+  const copy = () => {
+    navigator.clipboard.writeText(showValue);
+    setIsDialogDisplay(true);
+
+    // 3秒たったらダイアログを非表示に戻す
+    setTimeout((() => {
+      setIsDialogDisplay(false);
+    }).bind(this), 3000);
   }
 
   return (
     <>
-      {/* ダイアログ */}
-      <_Dialog isDisplay={isDialog}>
-        <dialog>
-          <span>■プレビュー</span>
-          <textarea readOnly value={previewValue} />
-
-          <span>■日付</span>
-          <input type="text" value={date} list="dateList" onChange={(e)=>{
-            setDate(e.target.value);
-          }}/>
-           <datalist id="dateList">{dataHistoryJsxList}</datalist>
-
-          <span>■所属</span>
-          <input type="text" value={affiliation} placeholder="例）CSC" list="affiliationList" onChange={(e)=>{
-            setAffiliation(e.target.value);
-          }}/>
-          <datalist id="affiliationList">{affiliationHistoryJsxList}</datalist>
-
-          <span>■名前</span>
-          <input type="text" value={name} placeholder="例）〇〇様" list="nameList" onChange={(e)=>{
-            setName(e.target.value);
-          }}/>
-          <datalist id="nameList">{nameHistoryJsxList}</datalist>
-
-          <button onClick={(e)=>{
-            setDialog(false);
-            setAffiliation('');
-            setName('');
-          }}>キャンセル</button>
-
-          <button onClick={(e)=>{
-            // 所属の入力があれば括弧をつける
-            if(affiliation != '') {
-              setShowValue(showValue + date + ' ' + affiliation + ')' + name + '\n\n' + selectedValue + '\n' + line + '\n');
-            } else {
-              setShowValue(showValue + date + ' ' + name + '\n\n' + selectedValue + '\n' + line + '\n');
-            }
-            setDialog(false);
-
-            if(!dateHistoryList.includes(date)){
-              dateHistoryList.push(date);
-              setDateHistoryList(dateHistoryList);
-            }
-            if(!affiliationHistoryList.includes(affiliation)){
-              affiliationHistoryList.push(affiliation);
-              setAffiliationHistoryList(affiliationHistoryList);
-            }
-            if(!nameHistoryList.includes(name)){
-              nameHistoryList.push(name);
-              setNameHistoryList(nameHistoryList);
-            }
-
-            // 初期化
-            setAffiliation('');
-            setName('');
-
-          }}>確定</button>
-        </dialog>
-      </_Dialog>
-
       {/* 左エリア */}
       <_Form>
-        <span>　■ベーステキスト</span>
-        <textarea value={baseValue} onChange={(e)=>{
-          setBaseValue(e.target.value);
-        }} />
+        <_CopyDialog isDisplay={isDialogDisplay}>クリップボードにコピーしました。</_CopyDialog>
+        <_FormFrame>
+          <_ItemName>■フォーマット</_ItemName>
+          <_FormatTextArea>
+            <textarea value={formatValue} onChange={(e) => {
+              setFormatValue(e.target.value);
+            }} />
+          </_FormatTextArea>
+          <_TextArea>
+            <span>■列区切り文字:</span>
+            <input value={columnReplaseValue} onChange={(e) => {
+              setColumnReplaseValue(e.target.value)
+            }}></input>
+          </_TextArea>
+          <_TextArea>
+            <span>■行区切り文字:</span>
+            <input value={recordReplaseValue} onChange={(e) => {
+              setRecordReplaseValue(e.target.value)
+            }}></input>
+          </_TextArea>
+          <_ItemName>■ベーステキスト</_ItemName>
+          <_BaseTextArea>
+            <textarea value={baseValue} onChange={(e) => {
+              setBaseValue(e.target.value);
+            }} />
+          </_BaseTextArea>
+        </_FormFrame>
       </_Form>
 
       {/* 中央エリア */}
-      <_Buttons>
-        <button onClick={()=>{
-          setDialog(true);
-          setSelectedValue(window.getSelection()?.toString());
-
-        }}>抽出</button>
-
-        <button onClick={()=>{
-           setShowValue('');
-        }}>リセット</button>
-      </_Buttons>
+      <_ButtonsArea>
+        <_Buttons isDisable={formatValue != '' && baseValue != ''}>
+          <button onClick={() => {
+            convert();
+          }}>変換</button>
+        </_Buttons>
+        <_Buttons isDisable={showValue != ''}>
+          <button onClick={() => {
+            copy();
+          }}>コピー</button>
+        </_Buttons>
+      </_ButtonsArea>
 
       {/* 右エリア */}
       <_Form>
-        <span>　■抽出結果</span>
-        <textarea value={showValue}/>
+        <_FormFrame>
+          <_ItemName>■出力結果</_ItemName>
+          <_ResultTextArea>
+            <textarea value={showValue} onChange={(e) => { }} />
+          </_ResultTextArea>
+        </_FormFrame>
       </_Form>
     </>
   );
@@ -141,88 +108,155 @@ const App = ()=> {
 
 export default App;
 
-// システム日付を初期値に設定
-const getSystemDate = () => {
-  	let today = new Date(); 
-  	const year = today.getFullYear();
-	  const month = today.getMonth()+1;
-  	const day = today.getDate();
-    return year + '/' + month + '/' + day;
-}
-
+// フォーム
 const _Form = styled.div`
-  background-color: #b0ee97;
+  background-color: #97eeee;
   display: inline-block;
   vertical-align: top;
   width: calc(50% - 40px);
+  grid-template-columns: minmax(${SystemUtil.FORM_MIN_WIDTH}px, 100%)1fr 1fr;
+  overflow-x: auto;
+  overflow-y: hidden;
   height: 100%;
-  & textarea {
-    resize:none;
-    margin-left: 10px;
-    margin-bottom: 10px;
+  box-sizing: border-box;
+  padding: ${SystemUtil.MARGIN_MIN_SIZE}px;
+`;
+
+// フォーム（枠）
+const _FormFrame = styled.div`
+  width: 100%;
+  height: 100%;
+  box-sizing: border-box;
+  border: 2px solid #ffffff83;
+`;
+
+// 項目名ラベル
+const _ItemName = styled.div`
+  box-sizing: border-box; 
+  margin-left: ${SystemUtil.MARGIN_MIN_SIZE}px;
+  margin-bottom: ${SystemUtil.MARGIN_MIN_SIZE}px;
+  width: calc(100% - 100px);
+  height: ${SystemUtil.ITEM_NAME_HEIGHT}px;
+  font-size: ${SystemUtil.FONT_SIZE}px;
+  font-weight: bold;
+`;
+
+// フォーマットテキストエリア
+const _FormatTextArea = styled.div`
+  width: 100%;
+  height: ${SystemUtil.FORMAT_TEXT_AREA_HEIGHT}px;
+  box-sizing: border-box;
+  & textarea{
+    resize: none;
+    margin-left: ${SystemUtil.MARGIN_MAX_SIZE}px;
+    margin-bottom: ${SystemUtil.MARGIN_MAX_SIZE}px;
+    width: calc(100% - 20px);
+    height: 200px;
+    box-sizing: border-box;
+  }
+`;
+
+// 区切り文字テキストエリア
+const _TextArea = styled.div` 
+  width: 50%;
+  height: ${SystemUtil.TEXT_BOX_AREA_HEIGHT}px;
+  display: inline-block;
+  box-sizing: border-box;
+  & span{
+    display: inline-block;
+    box-sizing: border-box; 
+    margin-left: ${SystemUtil.MARGIN_MIN_SIZE}px;
+    margin-bottom: ${SystemUtil.MARGIN_MIN_SIZE}px;
+    height: ${SystemUtil.TEXT_BOX_HEIGHT}px;
+    font-size: ${SystemUtil.FONT_SIZE}px;
+    font-weight: bold;
+  }
+  & input{
+    display: inline-block;
+    resize: none;
+    margin-left: ${SystemUtil.MARGIN_MAX_SIZE}px;
+    margin-bottom: ${SystemUtil.MARGIN_MIN_SIZE}px;
+    height: ${SystemUtil.TEXT_BOX_HEIGHT}px;
+    width: ${SystemUtil.TEXT_BOX_WIDTH}px;
+    box-sizing: border-box;
+ }
+`;
+
+// ベーステキストエリア
+const _BaseTextArea = styled.div`
+  width: 100%;
+  height: calc(100% - 260px);
+  & textarea{
+    resize: none;
+    margin-left: ${SystemUtil.MARGIN_MAX_SIZE}px;
+    margin-bottom: ${SystemUtil.MARGIN_MAX_SIZE}px;
     width: calc(100% - 20px);
     height: calc(100% - 30px);
     box-sizing: border-box; 
   }
-  & span {
-    font-size: 15px;
+`;
+
+// 出力結果テキストエリア
+const _ResultTextArea = styled.div`
+  width: 100%;
+  height: 100%;
+  box-sizing: border-box;
+  & textarea{
+    resize: none;
+    margin-left: ${SystemUtil.MARGIN_MAX_SIZE}px;
+    margin-bottom: ${SystemUtil.MARGIN_MAX_SIZE}px;
+    width: calc(100% - 20px);
+    height: calc(100% - 30px);
+    box-sizing: border-box; 
   }
 `;
 
-const _Buttons = styled.div`
-  background-color: #b0ee97;
-  display: inline-block;
+// 中央エリア
+const _ButtonsArea = styled.div`
+  background-color: #97eeee;
   vertical-align: top;
   width: 80px;
   height: 100%;
   display: inline-flex;
   flex-flow: column;
   justify-content: center;
+  box-sizing: border-box;
+`;
+
+// ボタン
+const _Buttons = styled.div<{
+  isDisable: boolean;
+}>`
   & button {
+    ${props => props.isDisable ? '' : StylesUtil.IS_DISABLE}
     width: 100%;
     margin-bottom: 20px;
   }
 `;
 
-const _Dialog = styled.div<{
+// コピーしましたダイアログ
+const _CopyDialog = styled.div<{
   isDisplay: boolean;
 }>`
-  display: ${props => props.isDisplay?'block':'none'};
-  background-color: #0000007f;
-  width: 100%;
-  height: 100%;
+  display: ${props => props.isDisplay ? 'block' : 'none'};
   position: absolute;
-  top: 0;
-  left: 0;
-  z-index: 10;
-  & span {
-    font-size: 15px;
-  }
-  & dialog {
-    background-color: white;
-    display: inline-block;
-    width: 50%;
-    height: 300px;
-    top: 50%;
-    left: 50%;
-    padding: 2%;
-    transform: translate(-50%,-50%);
-  }
-  & textarea {
-    resize:none;
-    width: 100%;
-    height: 100px;
-  }
-  & input {
-    width: 100%;
-    height: 20px;
-  }
-  & button {
-    width: 100px;
-    height: 30px;
-    margin-top: 5px;
-    margin-right: 5px;
-    bottom: 10px;
-  }
+  font-size: ${SystemUtil.FONT_SIZE}px;
+  background-color: #97c7ee;
+  line-height: 40px;
+  width: 250px;
+  height: 40px;
+  top: 52%;
+  left: 52%;
+  user-select: none;
   
+  // 3秒かけて透明にする
+  animation: fadein-keyframes 3s ease 0s forwards;
+  @keyframes fadein-keyframes {
+    0% {
+      opacity: 1;
+    }
+    100% {
+      opacity: 0;
+    }
+  }
 `;
---
+++ src/systemUtil.tsx
@@ -0,0 +1,24 @@
+namespace SystemUtil {
+    // 共通文字サイズ
+    export const FONT_SIZE = 15;
+    // 共通margin(小)
+    export const MARGIN_MIN_SIZE = 5;
+    // 共通margin(大)
+    export const MARGIN_MAX_SIZE = 10;
+
+    // 画面最小幅
+    export const FORM_MIN_WIDTH = 840;
+
+    // 項目名ラベルのエリアの高さ
+    export const ITEM_NAME_HEIGHT = 20;
+    // フォーマットテキストエリアの高さ
+    export const FORMAT_TEXT_AREA_HEIGHT = 210;
+    // 区切り文字テキストボックスエリアの高さ
+    export const TEXT_BOX_AREA_HEIGHT = 25;
+    // 区切り文字テキストボックスの高さ
+    export const TEXT_BOX_HEIGHT = 20;
+    // 区切り文字テキストボックスの幅
+    export const TEXT_BOX_WIDTH = 50;
+}
+
+export default SystemUtil;
